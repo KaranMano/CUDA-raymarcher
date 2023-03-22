ac47a5554e79dfe7f0b991464e7965104b2ca709
Karan Singh <karan.mnj.singh@gmail.com>
2023-03-22 11:39:43 +0530

    add stb for saving images

--- src/main.cu
+++ src/main.cu
@@ -1,12 +1,32 @@
 #include <iostream>
-#include <stdio.h>
-#include <time.h>
+#include <cmath>
+#include <algorithm>
+#include <stb_image_write.h>
 
-__global__ void renderVolume() {
-    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+#define IMAGE_SIZE 1080
+#define CHANNELS 1
+#define BLOCK_SIZE 16
+
+__global__ void renderVolume(unsigned char *image) {
+    int row = blockIdx.x * blockDim.x + threadIdx.x;
+    int col = blockIdx.y * blockDim.y + threadIdx.y;
+
+	image[IMAGE_SIZE * row + col] = 255;
 }
 
 int main() {
-    renderVolume<<<1,1>>>();
+    dim3 gridDim(std::ceil(IMAGE_SIZE / BLOCK_SIZE), std::ceil(IMAGE_SIZE / BLOCK_SIZE));
+    dim3 blockDim(BLOCK_SIZE, BLOCK_SIZE);
+    
+    unsigned char *image, *d_image;
+    image = (unsigned char *)malloc(IMAGE_SIZE * IMAGE_SIZE * CHANNELS * sizeof(unsigned char));
+    cudaMalloc(&d_image, IMAGE_SIZE * IMAGE_SIZE * CHANNELS * sizeof(unsigned char));
+    renderVolume<<<gridDim, blockDim>>>(d_image);
+    cudaMemcpy(image, d_image, IMAGE_SIZE * IMAGE_SIZE * CHANNELS * sizeof(unsigned char), cudaMemcpyDeviceToHost);
+    cudaFree(d_image);
+    
+    stbi_write_png("test.png", IMAGE_SIZE, IMAGE_SIZE, CHANNELS, image, IMAGE_SIZE * CHANNELS);
+
+    free(image);
     return 0;
 }
